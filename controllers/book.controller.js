cd9bc959a39c5977302684d57770347b9373ad64
atheo810 <atheos810@gmail.com>
2023-06-18 17:44:05 +0800

    feat: controller book

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,7 +1,80 @@
-const { PrismaClient } = require('@prisma/client');
-
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-class BookController{}
+class BookController {
+  static async listpage(req, res) {
+    const result = await prisma.book.findMany({});
+    res.render("page/list", { books: result });
+  }
+
+  static async detailpage(req, res) {
+    const result = await prisma.book.findUnique({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    res.render("pages/detail", { book: result });
+  }
+
+  static async createPage(req, res) {
+    res.render("pages/add");
+  }
+
+  static async store(req, res) {
+    await prisma.book.create({
+      data: {
+        name: req.body.name,
+        merk: req.body.merk,
+        qty: Number(req.body.qty),
+        available: req.body.available === "true" ? true : false,
+        price: Number(req.body.price),
+        img: req.body.img,
+        desc: req.body.description,
+      },
+    });
+    res.redirect("/book");
+  }
+
+  static async editPage(req, res) {
+    const result = await prisma.book.findUnique({
+      where: {
+        id: Number(req.body.id),
+      },
+    });
+    res.render("pages/edit", { book: result });
+  }
+
+  static async update(req, res) {
+    await prisma.book.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: {
+        name: req.body.name,
+        category: req.body.category,
+        qty: Number(req.body.qty),
+        available: req.body.available === "true" ? true : false,
+        price: Number(req.body.price),
+        img: req.body.img,
+        desc: req.body.description,
+      },
+    });
+
+    res.redirect("/book");
+  }
+
+  static async deletePage(req, res) {
+    await prisma.book.delete({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    res.redirect("/book");
+  }
+
+  static async aboutPage(req, res) {
+    res.render("pages/about");
+  }
+}
 
 module.exports = BookController;
--- routers/book.router.js
+++ routers/book.router.js
@@ -1,12 +1,25 @@
-const { Router } = require('express');
-const BookApiController = require('../controllers/bookApi.controller');
-const BookController = require('../controllers/book.controller');
+const { Router } = require("express");
+const BookApiController = require("../controllers/bookApi.controller");
+const BookController = require("../controllers/book.controller");
 
-const { getDetailBook, addBook, deleteBook, editBook, getBook } = BookApiController;
+const { getDetailBook, addBook, deleteBook, editBook, getBook } =
+  BookApiController;
+const { listpage, detailpage, editPage, aboutPage, createPage, deletePage, store, update } =
+  BookController;
 
 const router = Router();
+router.get("/book", listpage);
+router.get("/book/create", createPage);
+router.post("/book", store);
+router.get("/book/:id", detailpage);
+router.get("/book/:id/edit", editPage);
+router.post("/book/:id/edit", update);
+router.post("/book/:id/delete", deletePage);
 
+// About Page
+router.get("/book/about", aboutPage);
 
+// API routes
 router.get("/api/book", getBook);
 router.get("/api/book/:id", getDetailBook);
 router.post("/api/book", addBook);
